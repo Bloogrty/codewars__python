46a0d3abeb57d41b0e0d1e2c9ead10b9270de39d
Cristanto <cristanto.sidharta@gmail.com>
2025-06-07 22:53:45 +0700

    case 21

--- case021.py
+++ case021.py
@@ -1,2 +1,6 @@
-# name:
-# link:
+# name:How good are you really?
+# link:https://www.codewars.com/kata/5601409514fc93442500010b/train/python
+
+def better_than_average(class_points, your_points):
+  total_class = sum(class_points)/len(class_points)
+  return your_points > total_class
